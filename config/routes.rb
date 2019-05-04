f5152c786ebcc03d0212c32d6a75e9824d377895
koki.chujo <ec2-user@ip-172-31-34-153.us-east-2.compute.internal>
2019-05-04 04:11:07 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
  Rails.application.routes.draw do
+  get 'toppages/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
    root to: 'tasks#index'
-
+  root to: 'toppages#index'
   resources :tasks
 end
 
