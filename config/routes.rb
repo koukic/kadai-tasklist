89a88ba7c7f5c30f4e981815a620d1b356983149
koki.chujo <ec2-user@ip-172-31-34-153.us-east-2.compute.internal>
2019-05-04 06:00:37 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
  Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'toppages/index'
   get 'users/index'
   get 'users/show'
@@ -6,6 +9,12 @@
   get 'users/create'
    root to: 'tasks#index'
   root to: 'toppages#index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
   resources :tasks
 end
 
