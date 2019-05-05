69edb51df830ee01996eb0a7d1f98d4f37a69773
koki.chujo <ec2-user@ip-172-31-34-153.us-east-2.compute.internal>
2019-05-05 06:36:33 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
    before_action :set_task, only: [:show, :edit, :update, :destroy]
-
+   before_action :require_user_logged_in
      def index
       @tasks = Task.all
      end
@@ -14,11 +14,12 @@ class TasksController < ApplicationController
 
   def create
        @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      redirect_to tasks_url
     else
+       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -28,11 +29,12 @@ class TasksController < ApplicationController
   end
 
   def update
-
+  @task = current_user.tasks.build(task_params)
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to  tasks_url
     else
+       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
@@ -53,7 +55,7 @@ class TasksController < ApplicationController
    
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content,:status)
+    params.require(:task).permit(:content,:status,:user)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'toppages/index'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
   root to: 'toppages#index'
-   
-   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
@@ -17,6 +7,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   resources :tasks
+  
 end
 
 
